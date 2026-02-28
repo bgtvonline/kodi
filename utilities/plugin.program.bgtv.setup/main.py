7040f08f6e18ce711c645e0efb45dfd7bf73d5c7
BGTV Online <bgtv@bgtv.online>
2026-02-28 03:28:33 +0200

    v1.0.6: Fix settings - write to multi-instance path + legacy path

--- utilities/plugin.program.bgtv.setup/main.py
+++ utilities/plugin.program.bgtv.setup/main.py
@@ -2,6 +2,21 @@ import os
 import xbmc
 import xbmcgui
 import xbmcvfs
+import json
+
+def is_addon_installed(addon_id):
+    """Check if an addon is installed using JSON-RPC"""
+    try:
+        request = json.dumps({
+            "jsonrpc": "2.0",
+            "method": "Addons.GetAddonDetails",
+            "params": {"addonid": addon_id},
+            "id": 1
+        })
+        response = json.loads(xbmc.executeJSONRPC(request))
+        return 'result' in response
+    except:
+        return False
 
 def run():
     dialog = xbmcgui.Dialog()
@@ -24,28 +39,66 @@ def run():
         dialog.ok("Отказано", "Не въведохте парола. Опитайте отново.")
         return
 
-    # Inform user we are setting things up
+    # Check if pvr.hts is already installed
+    pvr_installed = is_addon_installed('pvr.hts')
+
+    if not pvr_installed:
+        dialog.ok(
+            "[COLOR red]BGTV[/COLOR] Съветник",
+            "Сега Kodi ще ви попита дали искате да инсталирате PVR клиента.\n\nМоля, натиснете [COLOR green]ДА[/COLOR] (Yes)!"
+        )
+        xbmc.executebuiltin('InstallAddon(pvr.hts)')
+        
+        pDialog = xbmcgui.DialogProgress()
+        pDialog.create("[COLOR red]BGTV[/COLOR] Съветник", "Изчакване PVR клиентът да се инсталира...")
+        
+        for i in range(60):
+            xbmc.sleep(1000)
+            pDialog.update(int(i * 100 / 60))
+            if is_addon_installed('pvr.hts'):
+                break
+            if pDialog.iscanceled():
+                pDialog.close()
+                dialog.ok("Отказано", "Инсталацията беше прекъсната.")
+                return
+        
+        pDialog.close()
+        
+        if not is_addon_installed('pvr.hts'):
+            dialog.ok("Грешка", "PVR клиентът не беше инсталиран.\nМоля стартирайте Съветника отново.")
+            return
+
+    # Now configure the addon
     pDialog = xbmcgui.DialogProgress()
-    pDialog.create("[COLOR red]BGTV[/COLOR] Съветник", "Инсталиране на PVR клиент...")
-    pDialog.update(10)
+    pDialog.create("[COLOR red]BGTV[/COLOR] Съветник", "Активиране на PVR клиента...")
+    pDialog.update(20)
 
-    # Enable the PVR addon (it should already be installed as a dependency)
     xbmc.executebuiltin('EnableAddon(pvr.hts)')
-    xbmc.sleep(3000)
-    pDialog.update(40, "Конфигуриране на сървъра...")
+    xbmc.sleep(2000)
+    pDialog.update(40, "Записване на настройките...")
 
-    # Build the settings XML content
-    # This directly writes to the Kodi userdata filesystem, 
-    # bypassing the broken setSetting() API for binary PVR addons
-    settings_xml = '''<?xml version="1.0" encoding="utf-8"?>
-<settings version="2">
+    # --- CRITICAL FIX ---
+    # Kodi 20+ (Nexus/Omega) uses MULTI-INSTANCE PVR settings.
+    # The old settings.xml in addon_data/pvr.hts/ is OBSOLETE for connection settings.
+    # Connection settings (host, user, pass) are now stored PER INSTANCE at:
+    #   addon_data/pvr.hts/instances/instance-1/settings.xml
+    # We must also write to the root settings.xml for backward compatibility.
+    
+    # Instance settings (where connection info actually lives in Kodi 20+)
+    instance_settings_xml = '''<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<settings version="1">
     <setting id="host">{host}</setting>
     <setting id="http_port">{http_port}</setting>
     <setting id="htsp_port">{htsp_port}</setting>
     <setting id="user">{user}</setting>
     <setting id="pass">{password}</setting>
-    <setting id="epg_async" default="true">true</setting>
-    <setting id="streaming_profile">default</setting>
+    <setting id="connect_timeout">10</setting>
+    <setting id="response_timeout">5</setting>
+    <setting id="streaming_profile"></setting>
+    <setting id="streaming_http">false</setting>
+    <setting id="pretuner_enabled">false</setting>
+    <setting id="epg_async">true</setting>
+    <setting id="dvr_playstatus">true</setting>
 </settings>'''.format(
         host='bgtv.pw',
         http_port='9981',
@@ -54,35 +107,60 @@ def run():
         password=password
     )
 
+    # Legacy root settings (for older Kodi or migration)
+    root_settings_xml = '''<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<settings version="1">
+    <setting id="host">{host}</setting>
+    <setting id="http_port">{http_port}</setting>
+    <setting id="htsp_port">{htsp_port}</setting>
+    <setting id="user">{user}</setting>
+    <setting id="pass">{password}</setting>
+    <setting id="epg_async">true</setting>
+</settings>'''.format(
+        host='bgtv.pw',
+        http_port='9981',
+        htsp_port='9982',
+        user=username,
+        password=password
+    )
+
+    addon_data = xbmcvfs.translatePath('special://profile/addon_data/pvr.hts/')
+    
+    # Write to ALL possible settings locations to cover every Kodi version
+    paths_to_write = [
+        # Modern Kodi 20+ instance path
+        (os.path.join(addon_data, 'instances', 'instance-1', 'settings.xml'), instance_settings_xml),
+        # Legacy root path (Kodi 19 and below, also migration)
+        (os.path.join(addon_data, 'settings.xml'), root_settings_xml),
+    ]
+    
     pDialog.update(60, "Записване на настройките...")
-
-    # Get the correct path for the PVR addon settings
-    settings_dir = xbmcvfs.translatePath('special://profile/addon_data/pvr.hts/')
-    settings_path = os.path.join(settings_dir, 'settings.xml')
-
-    # Ensure the directory exists
-    if not os.path.exists(settings_dir):
-        os.makedirs(settings_dir)
-
-    # Write the settings file directly
-    try:
-        with open(settings_path, 'w', encoding='utf-8') as f:
-            f.write(settings_xml)
-        xbmc.log("BGTV Setup: Successfully wrote settings to " + settings_path, xbmc.LOGINFO)
-    except Exception as e:
-        xbmc.log("BGTV Setup Error writing settings: " + str(e), xbmc.LOGERROR)
-        pDialog.close()
-        dialog.ok("Грешка", "Не можах да запиша настройките.\nГрешка: " + str(e))
-        return
+    
+    success = True
+    for filepath, content in paths_to_write:
+        dirpath = os.path.dirname(filepath)
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+        try:
+            with open(filepath, 'w', encoding='utf-8') as f:
+                f.write(content)
+            xbmc.log("BGTV Setup: Wrote settings to " + filepath, xbmc.LOGINFO)
+        except Exception as e:
+            xbmc.log("BGTV Setup Error writing to " + filepath + ": " + str(e), xbmc.LOGERROR)
+            success = False
 
     pDialog.update(90, "Почти готово...")
     xbmc.sleep(1000)
     pDialog.close()
 
-    # Ask user to restart Kodi
+    if not success:
+        dialog.ok("Грешка", "Не можах да запиша всички настройки. Моля опитайте отново.")
+        return
+
+    # Ask to restart
     restart = dialog.yesno(
         "Успех! ✅", 
-        "BGTV телевизията е настроена перфектно!\n\nСървър: bgtv.pw\nПотребител: {user}\n\nKodi трябва да се рестартира, за да се покажат каналите.\nДа го рестартирам ли сега?".format(user=username)
+        "BGTV е настроена!\n\nСървър: bgtv.pw\nПотребител: {user}\n\nKodi трябва да се рестартира.\nДа го затворя ли сега?".format(user=username)
     )
 
     if restart:
